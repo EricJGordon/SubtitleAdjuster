95623dcfe384c50f16ab9e956c5409d405cef30f
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-01-05 22:17:38 +0000

    Calculate sum of original number with offset of milliseconds/seconds/minutes/hours

--- time_format.py
+++ time_format.py
@@ -1,9 +1,9 @@
 class TimeFormat:
     def __init__(self, time_str):
-        self.hours = time_str[:2]
-        self.minutes = time_str[3:5]
-        self.seconds = time_str[6:8]
-        self.milliseconds = time_str[9:]
+        self.hours = int(time_str[:2])
+        self.mins = int(time_str[3:5])
+        self.secs = int(time_str[6:8])
+        self.millis = int(time_str[9:])
 
         # TODO: add_offset function
 
@@ -11,3 +11,21 @@ class TimeFormat:
         # print("test", self.hours, self.minutes, self.seconds, self.milliseconds)
         return '(just seconds: ' + self.seconds + ' )'
 
+    def offset_by(self, offset):
+        other = TimeFormat(offset)
+        temp = self.millis + other.millis
+        self.secs += temp // 1000
+        self.millis = temp % 1000
+
+        temp = self.secs + other.secs
+        self.mins += temp // 60
+        self.secs = temp % 60
+
+        temp = self.mins + other.mins
+        self.hours += temp // 60
+        self.mins = temp % 60
+
+        self.hours = (self.hours + other.hours) % 100
+        # hours not guaranteed to be correct in cases of overflow, but realistically not a problem
+
+        # combine and handle leading 0's
