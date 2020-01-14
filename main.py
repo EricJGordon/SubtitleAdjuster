eeaf2a02e1a8330d7d49d128a4bf4318ec75e67a
Eric Gordon <eric.gordon@ucdconnect.ie>
2020-01-14 23:33:27 +0000

    File name now taken as input instead of hardcoded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import fileinput
 from time_format import TimeFormat
 
@@ -7,7 +8,7 @@ def time_repl(match):
     return TimeFormat(match.group(0)).offset_by("00:00:08,123")
 
 
-for line in fileinput.input("subtitles.srt", inplace=True):
+for line in fileinput.input(sys.argv[1], inplace=True):
     line = re.sub(r"\d{2}:\d{2}:\d{2},\d{3}", time_repl, line)
     print(line, end='')
 
